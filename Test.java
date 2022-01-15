88170f3a4d7ebdcab012c548d0586335029bb176
vinshettigar <vinayakshettigar.2016@gmail.com>
2022-01-15 20:27:29 +0530

    bug113

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 class Test{
     public static void main(String args[]){
         System.out.print("Hello");
+<<<<<<< HEAD
         System.out.print("Good Morning");
+=======
+        System.out.print("Welcome to git");
+>>>>>>> f336841 (bug113)
     }
 }
